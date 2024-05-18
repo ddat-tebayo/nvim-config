a49d088143e7f66641c283a40cb10e6a75b2d9ec
Tran Tien Dat <trantiendat.dev@gmail.com>
2024-05-18 11:42:19 +0700

    feat: add plugins to enhance Neovim with modern IDE features

--- lua/plugins/configs/ufo.lua
+++ lua/plugins/configs/ufo.lua
@@ -34,7 +34,16 @@ end
 
 ufo.setup({
     provider_selector = function(bufnr, filetype, buftype)
-        return {'treesitter', 'indent'}
+        -- Option 2: nvim lsp as LSP client
+        -- Tell the server the capability of foldingRange,
+        -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+        return {'lsp', 'indent'}
+        
+        -- Option 3: treesitter as a main provider instead
+        -- (Note: the `nvim-treesitter` plugin is *not* needed.)
+        -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
+        -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+        -- return {'treesitter', 'indent'}
     end,
     fold_virt_text_handler = handler
 })
---
+++ lua/plugins/editor.lua
@@ -0,0 +1,156 @@
+return {
+    -- File explorer
+    {
+        'nvim-tree/nvim-tree.lua', 
+        config = function()
+          require('plugins.configs.nvim-tree')
+        end,
+    },
+
+    -- A highly extendable fuzzy finder over lists
+    -- 'BurntSushi/ripgrep' is required for live_grep and grep_string and is the first priority for find_files.
+    {
+        'nvim-telescope/telescope.nvim',
+        config = function()
+            require("plugins.configs.telescope")
+        end,
+        dependencies = {
+          -- Native telescope sorter to significantly improve sorting performance.
+          -- You have to install 'fzf', 'make' and 'gcc'
+          'nvim-telescope/telescope-fzf-native.nvim',
+          build = 'make',
+          config = function()
+            require("telescope").load_extension("fzf")
+          end,
+        }
+    },
+
+    -- The superior project management solution for neovim.
+    -- Access your recently opened projects from telescope!
+    {
+        'ahmedkhalf/project.nvim',
+        config = function()
+            require("project_nvim").setup {}
+        end,
+    },
+
+    -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
+    {
+        'folke/trouble.nvim',
+        opts = {},
+    },
+
+    -- A neovim lua plugin to help easily manage multiple terminal windows
+    {
+        'akinsho/toggleterm.nvim', version = "*",
+        config = function()
+            require('plugins.configs.toggleterm')
+        end,
+    },
+
+    -- Git integration for buffers
+    {
+        'lewis6991/gitsigns.nvim',
+        event = "BufRead",
+        config = function()
+            require("gitsigns").setup {}
+        end,
+    },
+
+    -- Neovim plugin for a code outline window
+    {
+        'stevearc/aerial.nvim',
+        opts = {
+            backends = { "treesitter", "lsp", "markdown" },
+            layout = {
+                width = 40,
+                default_direction = "prefer_left",
+            }
+        }
+    },
+
+    -- Smart and powerful comment plugin for neovim. Supports treesitter, dot repeat, left-right/up-down motions, hooks, and more
+    {
+        'numToStr/Comment.nvim',
+        opts = {}
+    },
+
+    -- Highlight, list and search todo comments in your projects
+    {
+        "folke/todo-comments.nvim",
+        opts = {}
+    },
+
+    -- Simple winbar/statusline plugin that shows your current code context
+    {
+        'SmiteshP/nvim-navic',
+        config = function()
+            require("nvim-navic").setup({
+                icons = require("utils.icons").kinds,
+                highlight = true,
+                lsp = {
+                  auto_attach = true,
+                  preference = nil,
+                }
+            })
+        end
+    },
+
+	-- Make Neovim's fold look modern and keep high performance.
+    {
+        'kevinhwang91/nvim-ufo',
+        event = "VeryLazy",
+        config = function()
+            require('plugins.configs.ufo')
+        end,
+        dependencies = {
+            'kevinhwang91/promise-async',
+            {
+                -- Status column plugin that provides a configurable 'statuscolumn' and click handlers
+                -- getting rid of folding level numbers in nvim-ufo
+                'luukvbaal/statuscol.nvim',
+                event = "VeryLazy",
+                opts = function()
+                    local builtin = require('statuscol.builtin')
+                    return {
+                      setopt = true,
+                      relculright = true,
+                      -- segments (sign -> line number + separator -> fold)
+                      segments = {
+                        { text = { ' %s' }, click = 'v:lua.ScSa' },
+                        {
+                          text = { builtin.lnumfunc, ' ' },
+                          condition = { true, builtin.not_empty },
+                          click = 'v:lua.ScLa',
+                        },
+                        { text = { builtin.foldfunc, '  ' }, click = 'v:lua.ScFa' },
+                      },
+                    }
+                end,
+            },
+        },
+    },
+
+    -- Color picker and highlighter
+    {
+        'uga-rosa/ccc.nvim',
+        event = "BufEnter",
+        config = function()
+            require('ccc').setup({
+                highlighter = {
+                    auto_enable = true,
+                    lsp = true,
+                }, 
+            })
+        end,
+    },
+
+    -- Displays a popup with possible keybindings of the command you started typing
+    {
+        'folke/which-key.nvim',
+        event = "VeryLazy",
+        config = function()
+            require('plugins.configs.whichkey')
+        end,
+    },
+}

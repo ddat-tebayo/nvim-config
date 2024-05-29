f6354466ef0a3fa26c25b4669ea2a057cebda217
Tran Tien Dat <trantiendat.dev@gmail.com>
2024-05-29 15:27:32 +0700

    merge treesitter into coding.lua with auto-complete and LSP config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,4 +1,36 @@
 return {
+  ---------- Syntax highlightings ----------
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("plugins.configs.treesitter")
+    end,
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require("lazy.core.loader").add_to_rtp(plugin)
+      require("nvim-treesitter.query_predicates")
+    end,
+    dependencies = {
+      -- Syntax aware text-objects, select, move, swap, and peek support.
+      {'nvim-treesitter/nvim-treesitter-textobjects'},
+
+      -- Show context of the current function
+      {
+        'nvim-treesitter-context',
+        event = "BufEnter",
+        opts = { max_lines = 3 }
+      }
+    }
+  },
+
+  --------------------------------------------------
+
   ---------- Language Server Protocol ----------
 
   -- Portable package manager for Neovim that runs everywhere Neovim runs. Easily install and manage LSP servers, DAP servers, linters, and formatters.
@@ -28,6 +60,7 @@ return {
       { "folke/neodev.nvim", opts = {} },
     }
   },
+
   --------------------------------------------------
 
   ---------- Auto completion ----------
@@ -61,6 +94,7 @@ return {
     dependencies = {
       --Set of preconfigured snippets for different languages.
       'rafamadriz/friendly-snippets',
+
       -- vscode-like pictograms for neovim lsp completion items
       'onsails/lspkind.nvim',
     }
@@ -79,9 +113,11 @@ return {
       })
     end,
   },
+
   --------------------------------------------------
 
   ---------- Others ----------
+
   -- Auto pairs
   {
     'windwp/nvim-autopairs',

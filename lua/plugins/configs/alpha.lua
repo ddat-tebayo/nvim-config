0867af95eb1f5ddd7b961c92be8c15e5c40501e6
Tran Tien Dat <trantiendat.dev@gmail.com>
2024-07-19 06:41:05 +0700

    feat: use custom highlight groups instead of default

--- lua/plugins/configs/alpha.lua
+++ lua/plugins/configs/alpha.lua
@@ -1,37 +1,38 @@
 local status, alpha = pcall(require, "alpha")
 if not status then
-  return
+    return
 end
 
+local custom_highlights = require('utils.custom_highlights')
 local dashboard = require("alpha.themes.dashboard")
+local dashboard_img = require("utils.dashboard_imgs")
 
 ----- Header section -----
 math.randomseed(os.time())
 
 local function pick_color()
-  local colors = { "RainbowDelimiterRed", "RainbowDelimiterYellow", "RainbowDelimiterGreen", "RainbowDelimiterBlue",
-    "RainbowDelimiterCyan", "RainbowDelimiterViolet" }
-  return colors[math.random(#colors)]
+    local colors = {"CustomRed", "CustomOrange", "CustomYellow", "CustomGreen", "CustomBlue", "CustomCyan", "CustomViolet, CustomWhite"}
+    return colors[math.random(#colors)]
 end
 
-dashboard.section.header.val = require("utils.dashboard_imgs").random()
+dashboard.section.header.val = dashboard_img.random()
 dashboard.section.header.opts.hl = pick_color()
 
 ----- Buttons section -----
 local leader = '<SPC>'
 
-local function button(hl_group, sc, txt, keybind, keybind_opts, opts)
+local function button(hl_group, sc, txt, keybind, keybind_opts, opts )
   local default_opts = {
     position = 'center',
     cursor = 5,
     width = 50,
     align_shortcut = 'right',
-    hl_shortcut = 'Number'
+    hl_shortcut = 'CustomOrange'
   }
 
-  opts = opts and vim.tbl_deep_extend('force', default_opts, opts) or default_opts
-
-  opts.hl = { { hl_group, 0, 3 } } -- only highlight icon
+  local opts = opts and vim.tbl_deep_extend('force', default_opts, opts) or default_opts
+  
+  opts.hl = {{hl_group, 0, 3}}
 
   opts.shortcut = sc
   local sc_after = sc:gsub('%s', ''):gsub(leader, '<leader>')
@@ -46,19 +47,18 @@ local function button(hl_group, sc, txt, keybind, keybind_opts, opts)
     opts.keymap = { 'n', sc_after, keybind, keybind_opts }
   end
 
-  return { type = 'button', val = txt, on_press = on_press, opts = opts }
+  return { type = 'button', val = txt, on_press = on_press, opts = opts}
 end
-
+  
 dashboard.section.buttons.val = {
-  button('RainbowDelimiterViolet', "r", "  Recently opened files", "<cmd>Telescope oldfiles<cr>"),
-  button('RainbowDelimiterGreen', "n", "  New file", ":ene <BAR> startinsert <CR>"),
-  button('RainbowDelimiterCyan', "f", "  Find file",
-    ":cd $HOME/Workspace | Telescope find_files hidden=true path_display=smart<CR>"),
-  button('RainbowDelimiterBlue', "t", "  Find text", ":Telescope live_grep path_display=smart<CR>"),
-  button('RainbowDelimiterYellow', "p", "  Switch to project", ":Telescope projects <CR>"),
-  button('RainbowDelimiterBlue', "u", "  Update plugins", ":Lazy sync<CR>"),
-  button('RainbowDelimiterRed', "q", "  Quit Neovim", ":qa<CR>"),
-  -- button('RainbowDelimiterOrange', leader .. " c f", "  Configuration", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>")
+  button('CustomViolet', "r", "  Recently opened files", "<cmd>Telescope oldfiles<cr>"),
+  button('CustomGreen', "n", "  New file", ":ene <BAR> startinsert <CR>"),
+  button('CustomCyan', "f", "  Find file", ":cd $HOME/Workspace | Telescope find_files hidden=true path_display=smart<CR>"),
+  button('CustomWhite', "t", "  Find text", ":Telescope live_grep path_display=smart<CR>"),
+  button('CustomYellow', "p", "  Switch to project", ":Telescope projects <CR>"),
+  button('CustomBlue', "u", "  Update plugins", ":Lazy sync<CR>"),
+  button('CustomRed', "q", "⏻  Quit Neovim", ":qa<CR>"),
+  -- button('CustomOrange', leader .. " c f", "  Configuration", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>")
 }
 
 ----- Footer section -----
@@ -71,11 +71,11 @@ local function footer()
   local version = vim.version()
   local nvim_version_info = "  v" .. version.major .. "." .. version.minor .. "." .. version.patch
   -----
-  return stats.count .. " plugins  " .. datetime .. nvim_version_info
+  return  stats.count .. " plugins  " .. datetime .. nvim_version_info
 end
 
 dashboard.section.footer.val = footer()
-dashboard.section.footer.opts.hl = "RainbowDelimiterGreen"
+dashboard.section.footer.opts.hl = "CustomGreen"
 
 ----- Quote or texts section -----
 local custom_texts = {
@@ -83,7 +83,7 @@ local custom_texts = {
   val = "󰀱 Coup de Burst at the ready 󱓞",
   opts = {
     position = "center",
-    hl = "RainbowDelimiterCyan",
+    hl = "CustomCyan",
   },
 }
 
@@ -98,7 +98,7 @@ dashboard.config.layout = {
   { type = "padding", val = 1 },
   custom_texts,
 }
-
+  
 dashboard.opts.opts.noautocmd = true
 
-alpha.setup(dashboard.config)
+require("alpha").setup(dashboard.config)
